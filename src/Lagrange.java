b39d5b42afca0a3625c4963388eb32591963d45c
CaioPinho9 <69582667+CaioPinho9@users.noreply.github.com>
2022-09-15 17:26:27 -0300

    misc: comments

--- src/Lagrange.java
+++ src/Lagrange.java
@@ -1,63 +1,101 @@
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Lagrange {
     public static void main(String[] args) {
-        int n = 0;
+        //Valor x de cada ponto, max 10
         float[] x = new float[10];
+        //Valor y de cada ponto, max 10
         float[] y = new float[10];
+
+        //Valor de x0
         float x0;
+        //Resultado de x0
         float y0 = 0;
+
+        //ax^2 + bx + c
         float a = 0;
         float b = 0;
         float c = 0;
-        int i = 0;
+
+        //Número de pontos = 3
         int u = 3;
+        //Número de elementos
+        int n = 0;
+
+        //Manter pedindo pontos S ou N
         String go = "S";
+
+        //Input
         Scanner scanner = new Scanner(System.in);
+
+        //Pedir o x0
         System.out.print("Digite o x: ");
         x0 = scanner.nextInt();
 
+        //Enquanto não mandar parar continue pedindo pontos
         while(go.equals("S")) {
-            for (; i < u; i++) {
+
+            //Pedir pelo menos 3 pontos
+            for (int i = 0; i < u; i++) {
+                //Pedir x e y de cada ponto
                 System.out.print("Digite o x"+i+": ");
                 x[i] = scanner.nextFloat();
                 System.out.print("Digite o y"+i+": ");
                 y[i] = scanner.nextFloat();
             }
+            //Confirmar se deseja continuar adicionando pontos
             System.out.print("Quer adicionar mais um ponto? (S/N)");
             go = scanner.nextLine();
             go = scanner.nextLine();
             go = go.toUpperCase(Locale.ROOT);
+
+            //Atualizar o número de elementos de acordo com o número de pontos.
             n = u - 1;
+            //Aumentar o número de pontos caso não tenha quebrado o loop.
             u++;
         }
 
+        //Lagrange
+        //Conhecido como Bubble ou Nested loop, loop dentro de loop
+        //Loop até o número de elementos
         for (int k = 0; k <= n; k++) {
+            //Valores das repostas, 1 para produto e 0 para soma
             float yk = 1;
             float ak = 1;
             float bk = 0;
             float ck = 1;
+
+            //Segundo loop até o número de elementos
             for (int j = 0; j <= n; j++) {
+                //Ignora se j == k
                 if (k != j) {
+                    //Cada conta é mantido o resultado para o proximo loop
                     yk *= (x0 - x[j]) / (x[k] - x[j]);
+
+                    //a = yk se o númerador fosse igual a 1
                     ak *= 1 / (x[k] - x[j]);
+
+                    //b = yk se o x0 fosse igual a 0
+                    //Depois que somar todos os númeradores é dividido pelo denominador
                     bk -= x[j];
+
+                    //c = yk se x0 fosse igual a 0
                     ck *= -x[j] / (x[k] - x[j]);
                 }
             }
-            for (int j = 0; j <= n; j++) {
-                if (k != j) {
-                    bk *= 1 / (x[k] - x[j]);
-                }
-            }
+            //Para o b especificamente é necessário somar todos os valores primeiro para depois dividir
+            //ak é o denominador de yk, então é possível utilizá-lo como divisor.
+            bk *= ak;
+
+            //Multiplicando pelo y desse index
             y0 += yk * y[k];
             a += ak * y[k];
             b += bk * y[k];
             c += ck * y[k];
 
         }
+        //Mostrar Resultado
         System.out.println("O y de x = 0 é " + y0);
         System.out.println(a + "x^2 + " + b + "x + " + c);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,106 @@
 public class Main {
     public static void main(String[] args) {
+        //Último index
         int n = 9;
+        //Index inicial
         int m = 3;
+        //Resultado
         float a;
 
+        // Escolha se vai utilizar Soma ou Produto
+        // (m, n, 0) -> 0 significa que é a equação externa
+        // Enquanto 1 é usado para quando tem um somatório ou produto dentro do outro
+        // a = Sum(m, n, 0)
         a = Pro(m, n, 0) * 0.25f;
+        // Mostrar Resultado
         System.out.println(a);
     }
 
     public static float Sum(int m, int n, int index) {
+        //Resultado da soma
         float a = 0;
+
+        //De m até n
         for (int i = m; i<=n; i++) {
             if (index == 0) {
+                //Somatório externo
+                //Troque o metódo pela equação que deseja utilizar
                 a += fourJ(i);
             } else {
+                //Somatório interno
+                //Troque o metódo pela equação que deseja utilizar
                 a += twoJ(i);
             }
         }
+        //Retorna o resultado
         return a;
     }
 
     public static float Pro(int m, int n, int index) {
+        //Resultado do produto
         float a = 1;
+
+        //De m até n
         for (int i = m; i<=n; i++) {
             if (index == 0) {
+                //Somatório externo
+                //Troque o metódo pela equação que deseja utilizar
                 a *= k3(i);
             } else {
+                //Somatório interno
+                //Troque o metódo pela equação que deseja utilizar
                 a *= h2(i);
             }
         }
         return a;
     }
 
+    //(i - 1)^2
     public static float iMinus1(int i) {
         return (float) Math.pow((i - 1), 2);
     }
 
+    //i^2
     public static float ii(int i) {
         return i*i;
     }
 
+    //i^3 - i^2 + 1
     public static float i3i2(int i){
         return (float) (Math.pow(i, 3) - Math.pow(i, 2) + 1);
     }
 
+    //7i
     public static float sevenI(int i) {
         return i*7;
     }
 
+    //(3 - i)^2
     public static float threeMinusI(int i) {
         return (float) Math.pow((3-i),2);
     }
 
+    //(4j - 3) * j^2
     public static float fourJ(int j) {
         return (float) ((4*j - 3) * Math.pow(j, 2));
     }
 
+    //2j
     public static float twoJ(int j) {
         return (float) 2 * j;
     }
 
+    //k^3 - (2/3) * Produto interno(4, 2, 1)
     public static float k3(int k) {
         return (float) (Math.pow(k, 3) - (2/3) * Pro(4,2,1));
     }
 
+    //h^2 - 1
     public static float h2(int h) {
         return (float) Math.pow(h, 2) - 1;
     }
 
+    //(3 - i^2)^2
     public static float threeMinusI2(int i) {
         return (float) Math.pow((3 - Math.pow(i, 2)), 2);
     }
